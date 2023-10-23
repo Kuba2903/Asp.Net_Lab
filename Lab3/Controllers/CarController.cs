8265b8abcce83776f19f6b3c039e038273ec7813
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2023-10-23 20:42:08 +0200

    trzeci commit

---
+++ Lab3/Controllers/CarController.cs
@@ -0,0 +1,105 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class CarController : Controller
+    {
+
+        static Dictionary<int,Car> _cars = new Dictionary<int,Car>();
+        
+        public IActionResult Index()
+        {
+            return View(_cars);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Car car)
+        {
+            if(ModelState.IsValid)
+            {
+                int id = _cars.Keys.Count != 0 ? _cars.Keys.Max() : 0;
+                car.Id = id + 1;
+                _cars.Add(car.Id, car);
+                return RedirectToAction("Index");
+            }else
+                return View(car);
+        }
+
+        
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            if (_cars.ContainsKey(id))
+            {
+                return View(_cars[id]);
+            }
+            else
+            {
+                return NotFound();
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Car editedCar)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_cars.ContainsKey(editedCar.Id))
+                {
+                    _cars[editedCar.Id] = editedCar;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return View(editedCar);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if(_cars.ContainsKey(id))
+                return View(_cars[id]);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            if (_cars.ContainsKey(id))
+            {
+                _cars.Remove(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_cars.ContainsKey(id))
+                return View(_cars[id]);
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ Lab3/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Car
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nie podano nazwy modelu")]
+        public string Model { get; set; }
+        [Required(ErrorMessage = "Nie podano nazwy producenta")]
+        public string Producent { get; set; }
+        [Required(ErrorMessage ="Nie podano pojemności silnika")]
+        public decimal PojemmoscSilnika { get; set; }
+        [Required(ErrorMessage ="Nie podano mocy")]
+        public int Moc { get; set; }
+        [Required(ErrorMessage ="Nie podano rodzaju silnika")]
+        public string RodzajSilnika { get; set; }
+        [Required(ErrorMessage ="Nie podano numeru rejestracyjnego")]
+        public string NrRejestracyjny { get; set; }
+        [Required(ErrorMessage ="Nie podano nazwy właściciela")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",ErrorMessage ="Niepoprawna nazwa")]
+        public string Wlasciciel { get; set; }
+
+        [HttpGet]
+        public String Edit(int? id)
+        {
+            return "Edycja " + id;
+        }
+    }
+}
